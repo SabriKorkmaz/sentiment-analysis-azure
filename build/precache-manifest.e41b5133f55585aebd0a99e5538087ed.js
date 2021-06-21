8b238ad038dca476cf647036c91b8506bca1f767
sabri <sabri_51@hotmail.com>
2021-06-21 16:37:44 +0300

    initial react

---
+++ build/precache-manifest.e41b5133f55585aebd0a99e5538087ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "00140b3a0e9e888a8c6c",
+    "url": "/static/css/main.6a459636.chunk.css"
+  },
+  {
+    "revision": "00140b3a0e9e888a8c6c",
+    "url": "/static/js/main.00140b3a.chunk.js"
+  },
+  {
+    "revision": "8160482793cdacdcec6a",
+    "url": "/static/css/1.84532ae8.chunk.css"
+  },
+  {
+    "revision": "8160482793cdacdcec6a",
+    "url": "/static/js/1.81604827.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "180d967a7a65a95eab90465847fa68a0",
+    "url": "/index.html"
+  }
+];
